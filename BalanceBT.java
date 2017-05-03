4f30ecfe85ab293ab7c400a701e5d4878a4f5a01
Avinash <akka@usc.edu>
2017-05-02 19:51:27 -0700

    Update BalanceBT.java

--- BalanceBT.java
+++ BalanceBT.java
@@ -1,2 +1,31 @@
 //Q:4.1
 //Function to check if a binary tree is balanced.
+
+class BalanceBT
+{
+  
+  public static boolean Solution(Node head)
+  {
+    if(head == null)
+      return false;
+    
+    if(head.left == null && head.right == null)
+    {
+      return false;
+    }
+    
+    int leftHeight = getHeight(head.left);
+    int rightHeight = getHeight(head.right);
+    
+    if(Math.abs(leftHeight - rightHeight) > 1)
+      return false;
+    else
+      return true;   
+  }
+  
+  public static int getHeight(Node current)
+  {
+    
+  }
+  
+}
